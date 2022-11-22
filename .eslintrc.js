bc920312446b453189a42be7b4ddb14ed4f2ecfe
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2022-11-23 00:53:57 +0530

    changes

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parserOptions: {
+    project: './tsconfig.json', // Specify it only for TypeScript files
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    'import/first': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+  },
+};
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
+import path from 'path';
 import authRouter from './routes/authRouter';
 import productRouter from './routes/productRouter';
 
 const app: Express = express();
-const base_url: string = process.env.BASE_PATH;
+const BASE_URL: string = process.env.BASE_PATH;
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -16,12 +17,12 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(`${base_url}/auth`, authRouter);
-app.use(`${base_url}/products`, productRouter);
+app.use(`${BASE_URL}/auth`, authRouter);
+app.use(`${BASE_URL}/products`, productRouter);
 
-app.get(`${base_url}/ishealthy`, (req: Request, res: Response) => {
+app.get(`${BASE_URL}/ishealthy`, (req: Request, res: Response) => {
   res.status(200).json({ message: 'I am Healthy', status: 200 });
 });
 
